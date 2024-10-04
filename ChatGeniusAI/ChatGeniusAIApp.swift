e4bb228e618b18d832ad7451f7e9a544b214d959
Minhaj Ali Khan <minhajalikhan909@gmail.com>
2024-10-04 06:10:46 -0700

    first commit

---
+++ ChatGeniusAI/ChatGeniusAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGeniusAIApp.swift
+//  ChatGeniusAI
+//
+//  Created by Apple on 04/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatGeniusAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
